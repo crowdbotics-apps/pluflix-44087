07c314ff36eedbfa83751ebd55dc5329edef66b3
Crowdbotics <team@crowdbotics.com>
2023-10-21 20:26:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pluflix-44087",
+  url: "https://pluflix-44087.botics.co",
+  api: "https://pluflix-44087.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
